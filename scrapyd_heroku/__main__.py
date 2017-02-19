82e56a20a5b5b885efe805ca85f9460ff34e3a30
jxltom <jxltom@gmail.com>
2017-02-19 22:09:58 +0800

    Fixed an issur where path of app.py is not correctly parsed

--- scrapyd_heroku/__main__.py
+++ scrapyd_heroku/__main__.py
@@ -1,11 +1,14 @@
+from os.path import join, dirname
 from sys import argv
 
 from twisted.scripts.twistd import run
 
+import scrapyd_heroku
+
 
 def main():
     """Entrypoint for script or exe."""
-    argv[1:1] = ['-n', '-y', 'app.py']
+    argv[1:1] = ['-n', '-y', join(dirname(scrapyd_heroku.__file__), 'app.py')]
     run()
 
 
