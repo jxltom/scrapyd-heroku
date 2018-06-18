f1501bbc2a765d790059a3315d7e7300ea32a4d7
Unknown <jxltom@gmail.com>
2018-06-18 14:35:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,108 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+import io
+import os
+import sys
+from shutil import rmtree
 
-from scrapyd_heroku import __version__, __author__
+from setuptools import find_packages, setup, Command
 
+
+NAME = 'scrapyd-heroku'
+DESCRIPTION = 'Wrapper for running Scrapyd in Heroku or locally as a service'
+URL = 'https://github.com/jxltom/scrapyd-heroku'
+EMAIL = 'jxltom@gmail.com'
+AUTHOR = 'jxltom'
+VERSION = None
+REQUIRES_PYTHON = '>=3.6.0'
+REQUIRED = [
+    'scrapyd'
+]
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME.replace('-', '_'), '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='scrapyd-heroku',
-    version=__version__,
-    description='A wrapper for running Scrapyd in Heroku '
-                'or in console as normal Scrapyd service',
-    long_description=
-    'Go to https://github.com/jxltom/scrapyd-heroku/ for more information.',
-    author=__author__,
-    author_email='jxltom@gmail.com',
-    url='https://github.com/jxltom/scrapyd-heroku/',
-    license='MIT',
-
-    include_package_data=True,
-
-    packages=find_packages(),
-    install_requires=[
-        'scrapy>=1.3.2',
-        'scrapyd>=1.2.0a1',
-    ],
-
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
     entry_points={
-        'console_scripts': [
-            'scrapyd-heroku = scrapyd_heroku.__main__:main'
-        ],
+        'console_scripts': ['scrapyd-heroku=scrapyd_heroku.manage:main'],
     },
-
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
