edf40c6ccc6733e995b1a41b8ca755446d4e87af
Unknown <jxltom@gmail.com>
2018-06-18 13:54:34 +0800

    Update bind_address

--- scrapyd_heroku/app.py
+++ scrapyd_heroku/app.py
@@ -9,9 +9,11 @@ def get_application(config=None):
     if config is None:
         config = Config()
         # Override http_port by $PORT environment variable in Heroku.
+        # Override bind_address to 0.0.0.0 if $PORT exists
         # Note that the http_port has to be a string intead of int.
         config.cp['scrapyd'].update(
-            http_port=os.environ.get('PORT', config.get('http_port'))
+            http_port=os.environ.get('PORT', config.get('http_port')),
+            bind_address='0.0.0.0' if os.environ.get('PORT') else config.get('bind_address')
         )
 
     apppath = config.get('application', 'scrapyd.app.application')
